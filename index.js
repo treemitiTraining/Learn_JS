e0901a589b025fb0ee3b8fad1cdb95422e9801e3
treemitiTraining <treemiti.training@gmail.com>
2024-12-06 17:03:19 +0530

    conditionalStatements

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-let a = 20;
-let b = 10;
+let userInput = prompt('Write fruite name');
+// console.log(userInput);
+
+
+
+switch (userInput) {
+    case "index":
+        console.log(userInput + " = " + "100kg");
+        break;
+    case "Kiwi":
+        console.log(userInput + " = " + '200kg');
+        break;
+    default:
+        console.log("No fruit available");
+        break;
+}
 
 
 /*
+ (key === value){
 
-b=20;
-a=10;
+ }
 
-*/ 
+*/
--- notes/4.ConditionalStatements.js
+++ notes/4.ConditionalStatements.js
@@ -5,4 +5,20 @@
 // }
 
 
+// if we want to take user input, we will use the below function
+// string:>     prompt();
+// Number:>    +prompt(); 
 
+
+
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+*/ 
